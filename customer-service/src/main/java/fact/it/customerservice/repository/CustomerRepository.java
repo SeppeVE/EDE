4a3a1be38b3b2740cd64aed286e868ad04489ddd
Seppe Van Eynde <r0882108@student.thomasmore.be>
2023-12-20 15:36:12 +0100

    id

--- customer-service/src/main/java/fact/it/customerservice/repository/CustomerRepository.java
+++ customer-service/src/main/java/fact/it/customerservice/repository/CustomerRepository.java
@@ -4,10 +4,7 @@ import fact.it.customerservice.model.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
     List<Customer> findByCustomerNr(String customerNr);
-
-    Optional<Customer> getByCustomerNr(String nr);
 }
